e115d81c94e37bc97fafea5e5e895f16fd6dc047
Mohammad Sakib <moh4mmadsakib@gmail.com>
2023-02-19 03:05:03 +0600

    Jenkins & AWS update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+cd ${WORKSPACE}
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
 docker build -t ${IMAGE_REPO_NAME}:${IMAGE_TAG} .
 docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:${IMAGE_TAG}
